40694223de453291efa30517490b3345267acb78
MXblade <cnmxcode@163.com>
2018-06-06 16:19:43 +0800

    change typechange.py

--- PythonTools/typechange.py
+++ PythonTools/typechange.py
@@ -11,6 +11,15 @@ def typechange(filename, targetfilename,datatype):
     file = open(filename,"r",encoding=result["encoding"])
     content = file.readlines()
     file = open('../files/testtxt/datatype_target.txt', 'w', encoding=datatype)
+    file.writelines(content)
+    file.close()
 
+originpath = '../files/testtxt/datatype_origin.txt'
+targetpath = '../files/testtxt/datatype_target.txt'
+typechange(originpath, targetpath, 'gbk')
 
-assert typechange('../files/testtxt/datatype_origin.txt', '../files/testtxt/datatype_target.txt', 'utf-8')
+file = open(targetpath, "rb")#要有"rb"，如果没有这个的话，默认使用gbk读文件。
+buf = file.read()
+result = chardet.detect(buf)
+file.close()
+print(result)
